ac84ab88d33e0306194c53ff6b051bdf70b85f53
balu <balu@mobile>
2023-04-01 19:51:56 +0200

    fixing url to allow addon.a-pass.de to be accessed but not addon.a-pass.de/#*

--- bg.js
+++ bg.js
@@ -3,27 +3,30 @@ console.log("-- inside js file --");
 function logURL(requestDetails) {
     try {
         const url = new URL(requestDetails.url);
-        //console.log(`Loading: ${requestDetails.url}`);
+        console.log(`Loading: ${requestDetails.url}`);
         //console.log(`url ${url}`)
         const hash = url.hash.substring(1);
         //console.log(`hash ${hash}`)
         const hash_dec = decodeURI(hash)
-        console.log(`hash_dec ${hash_dec}`)
-        //const json = JSON.parse(hash_dec) 
-        //console.log(`json ${json}`)
-        hash_url = new URL("https://addon.a-pass.de/?"+hash_dec);
+        //console.log(`hash_dec ${hash_dec}`)
+        const hash_url = new URL("https://addon.a-pass.de/?"+hash_dec);
         //console.log(`hash_url ${hash_url}`)
-      return { redirectUrl: hash_url.searchParams.get('url') };
+        if (hash_url.searchParams.get('url') != null){
+            return { redirectUrl: hash_url.searchParams.get('url') };
+        } else {
+            console.log("Could not get url");
+            return { redirectUrl: "https://addon.a-pass.de/error" };
+        }
     }
     catch(err) {
         console.log(err);
-        return { redirectUrl: "http://web.de" };
+        return { redirectUrl: "https://addon.a-pass.de/error" };
     }
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   logURL,
-  {urls: ["*://a-pass.de/*", "*://*.a-pass.de/*"]},
+  {urls: ["*://a-pass.de/#*", "*://*.a-pass.de/#*"]},
   ["blocking"]
 );
 
